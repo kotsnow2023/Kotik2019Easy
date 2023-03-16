7fb68513337c50ee73020820998af753e56b3b5c
kotsnow2023 <128006585+kotsnow2023@users.noreply.github.com>
2023-03-16 12:10:15 +1000

    test

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ bot = commands.Bot(command_prefix="*", intents=disnake.Intents.all())
 async def kotik(interaction: disnake.AppCmdInter):
   await interaction.send("Котик крутой")
 
-bot.run(token)
+bot.run(MTA4NTY4NjE2Njk5NDU2NzI4OQ.GDTwhv.F2Cy5Q-dJSL6zC47GJVQL4ZWrN52VZOpkmtUrU)
